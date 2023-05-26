1bcde19f1081529d1841c0ddc5eb56d75d63146b
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-05-26 17:50:51 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-indigo-500 via-sky-500  to-emerald-500 h-full w-full">
+        <h1>Hello.</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
