99ca61dcd871d4e505db2b2bb0bead2c2ae29701
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-05-27 17:29:53 -0400

    updates to using react. doh!

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-function App() {
-  setTimeout(() => {
-    const element = document.getElementById("imJonathon");
-    element?.classList.remove("hidden");
-    element?.classList.add("animate-slideInFromLeft", "flex");
-  }, 1000);
+import { useState } from "react";
 
+function App() {
+  const [isHiAnimationComplete, setIsHiAnimationComplete] = useState(false);
+  const handleAnimationEnd = () => {
+    setIsHiAnimationComplete(true);
+  };
   return (
     <>
-      <div className="m-16 columns-1 justify-items-center grid gap-6">
-        <div className="animate-slideInFromRight flex items-center justify-center text-[50px] border text-slate-900 border-slate-700 p-8 w-[150px] h-[150px]">
+      <div
+        id="container"
+        className="m-16 columns-1 justify-items-center grid gap-6"
+      >
+        <div
+          onAnimationEnd={handleAnimationEnd}
+          className="animate-slideInFromRight flex items-center justify-center text-[50px] shadow-lg text-slate-900 p-8 w-[150px] h-[150px]"
+        >
           hi!
         </div>
         <div
-          id="imJonathon"
-          className="justify-center text-[50px] text-slate-900 p-16 hidden"
+          className={` ${
+            isHiAnimationComplete ? "animate-slideInFromLeft" : "hidden"
+          } justify-center text-[50px] text-slate-900 p-16`}
         >
-          i'm jonathon.
+          i&apos;m jonathon
+        </div>
+        <div className="justify-center text-[50px] text-slate-900 p-16 hidden">
+          here are some of my projects
         </div>
       </div>
     </>
   );
 }
-
 export default App;
