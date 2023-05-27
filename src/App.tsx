02045e19fc81b24a398d216503bd1489db8c7b49
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-05-27 19:57:59 -0400

    restarting vscode after adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Introduction from "./pages/introduction";
 
 function App() {
-  const [isHiAnimationComplete, setIsHiAnimationComplete] = useState(false);
-  const handleAnimationEnd = () => {
-    setIsHiAnimationComplete(true);
-  };
   return (
-    <>
-      <div
-        id="container"
-        className="m-16 columns-1 justify-items-center grid gap-6"
-      >
-        <div
-          onAnimationEnd={handleAnimationEnd}
-          className="animate-slideInFromRight flex items-center justify-center text-[50px] shadow-lg text-slate-900 p-8 w-[150px] h-[150px]"
-        >
-          hi!
-        </div>
-        <div
-          className={` ${
-            isHiAnimationComplete ? "animate-slideInFromLeft" : "hidden"
-          } justify-center text-[50px] text-slate-900 p-16`}
-        >
-          i&apos;m jonathon
-        </div>
-        <div className="justify-center text-[50px] text-slate-900 p-16 hidden">
-          here are some of my projects
-        </div>
-      </div>
-    </>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Introduction />} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 export default App;
