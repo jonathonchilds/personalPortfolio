c308b1aaf671647342e955c9c81ec829dbc07f7e
Jonathon Childs <jonathon.k.childs@gmail.com>
2023-09-17 09:28:02 -0400

    fully mobile friendly

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,25 +14,29 @@ export default function Contact() {
   };
 
   return (
-    <div className="h-screen relative flex items-center justify-center">
+    <div className="h-screen relative flex items-center justify-center md:px-0 px-28 md:pt-0 pt-56 md:mb-0 mb-56">
       <div className="flex flex-col space-y-6 text-center">
         <a href="https://github.com/jonathonchilds/personalPortfolio/blob/trunk/src/App.tsx">
-          <button className="primaryBtn">Source Code For This Portfolio</button>
+          <button className="primaryBtnContactPage">
+            Source Code For This Portfolio
+          </button>
         </a>
-        <p className="text-2xl tracking-widest">(727) 967-3889</p>
-        <p className="text-2xl tracking-widest">jonathon.k.childs@gmail.com</p>
-        <p className="text-2xl tracking-widest">
+        <p className="md:text-2xl tracking-widest">(727) 967-3889</p>
+        <p className="md:text-2xl tracking-widest">
+          jonathon.k.childs@gmail.com
+        </p>
+        <p className="md:text-2xl tracking-widest">
           Tampa / Saint Petersburg, Florida
         </p>
         <p className="text-sm text-gray-500">
-          (This form will auto-fill your system's default email provider
-          after you submit!)
+          (This form will auto-fill your system's default email provider, after
+          you submit!)
         </p>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2 w-fit mx-auto"
+          className="flex flex-col md:space-y-2 space-y-6 w-fit mx-auto"
         >
-          <div className="flex space-x-2">
+          <div className="flex md:flex-row flex-col md:space-x-2 md:space-y-0 space-y-6">
             <input
               {...register("name")}
               placeholder="Name"
